86d38e40dd6809b1a7a33fd9a1aaecf99d7b0057
Dárius Poklemba <darius.poklemba@student.tuke.sk>
2022-04-11 12:06:26 +0200

    update

--- djangoProjectDiplo/settings.py
+++ djangoProjectDiplo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoProjectDiplo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'static']
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
